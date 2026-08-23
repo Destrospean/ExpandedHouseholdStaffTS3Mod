387ec353f1f8773d44dd282b0bb70c381a34850a
CreatorAndrew <andrew.noah.mitchell@gmail.com>
2026-08-23 09:46:08 -0500

    More fixes to STBLs

--- scripts/GenerateSTBLs.bat
+++ scripts/GenerateSTBLs.bat
@@ -1,5 +1,6 @@
 @echo off
 pushd "%~dp0"
+del ..\resources\*.stbl
 for %%i in (..\strings\*.yaml) do (
     ..\tools\STBLize+\STBLize+.exe "..\strings\%%i" -d ..\resources -nu
 )
--- scripts/GenerateSTBLs.sh
+++ scripts/GenerateSTBLs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd "${0%/*}"
+rm ../resources/*.stbl
 for i in ../strings/*.yaml; do
     mono ../tools/STBLize+/STBLize+.exe $i -d ../resources -nu
 done
