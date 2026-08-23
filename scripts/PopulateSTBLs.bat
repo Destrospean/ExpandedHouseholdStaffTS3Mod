bbb316da5810c250ed72f0aa3dfd93b118af67c5
CreatorAndrew <andrew.noah.mitchell@gmail.com>
2026-08-23 15:03:10 -0500

    More STBL changes

--- scripts/PopulateSTBLs.bat
+++ scripts/PopulateSTBLs.bat
@@ -1,13 +1,12 @@
 @echo off
 pushd "%~dp0"
-
-set MOD=..\strings\ServantRoleSims
+set STBL_PREFIX=..\strings\ServantRoleSims
 set MAIN_LANG=ENG_US
-set LIST=ENG_US CHS_CN CHT_CN CZE_CZ DAN_DK DUT_NL FIN_FI FRE_FR GER_DE GRE_GR HUN_HU ITA_IT JPN_JP KOR_KR NOR_NO POL_PL POR_PT POR_BR RUS_RU SPA_ES SPA_MX SWE_SE THA_TH
-
-for %%i in (%LIST%) do (
-    if not exist %MOD%_%%i.yaml (
-        copy %MOD%_%MAIN_LANG%.yaml %MOD%_%%i.yaml
+for %%i in (ENG_US CHS_CN CHT_CN CZE_CZ DAN_DK DUT_NL FIN_FI FRE_FR GER_DE GRE_GR HUN_HU ITA_IT JPN_JP KOR_KR NOR_NO POL_PL POR_PT POR_BR RUS_RU SPA_ES SPA_MX SWE_SE THA_TH) do (
+    if exist %STBL_PREFIX%_%%i.yaml (
+        ..\tools\STBLize+\STBLize+.exe %STBL_PREFIX%_%MAIN_LANG%.yaml -a %STBL_PREFIX%_%%i.yaml
+    ) else (
+        copy %STBL_PREFIX%_%MAIN_LANG%.yaml %STBL_PREFIX%_%%i.yaml
     )
 )
 popd
--- scripts/PopulateSTBLs.sh
+++ scripts/PopulateSTBLs.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 cd "${0%/*}"
-
-MOD=../strings/ServantRoleSims
+STBL_PREFIX=../strings/ServantRoleSims
 MAIN_LANG=ENG_US
-
 for i in ENG_US CHS_CN CHT_CN CZE_CZ DAN_DK DUT_NL FIN_FI FRE_FR GER_DE GRE_GR HUN_HU ITA_IT JPN_JP KOR_KR NOR_NO POL_PL POR_PT POR_BR RUS_RU SPA_ES SPA_MX SWE_SE THA_TH; do
-    if [ ! -f ${MOD}_${i}.yaml ]; then
-        cp ${MOD}_${MAIN_LANG}.yaml ${MOD}_${i}.yaml
+    if [ -f ${STBL_PREFIX}_${i}.yaml ]; then
+        mono ../tools/STBLize+/STBLize+.exe ${STBL_PREFIX}_${MAIN_LANG}.yaml -a ${STBL_PREFIX}_${i}.yaml
+    else
+        cp ${STBL_PREFIX}_${MAIN_LANG}.yaml ${STBL_PREFIX}_${i}.yaml
     fi
 done
