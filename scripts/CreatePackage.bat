35376d72e80a9cbee5e6ab4e820a101290da3321
CreatorAndrew <andrew.noah.mitchell@gmail.com>
2026-08-27 13:28:42 -0500

    Fixes for building on Windows

---
+++ scripts/CreatePackage.bat
@@ -0,0 +1,15 @@
+@echo off
+pushd "%~dp0"
+
+set modName=%1
+set dllPath=%2
+set projectDir=%3
+set defaultPath=%4
+
+set "dllPath=%dllPath:\=/%"
+set "projectDir=%projectDir:\=/%"
+set "defaultPath=%defaultPath:\=/%"
+
+..\tools\TS3BuildTool\ts3buildtool.exe -modName=%modName% -dllPath=%dllPath% -projectDir=%projectDir% -defaultPath=%defaultPath%
+
+popd
