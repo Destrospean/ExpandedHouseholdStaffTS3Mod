327a2ce7ee5af57adce51266003e533b8ae5d23a
CreatorAndrew <andrew.noah.mitchell@gmail.com>
2026-09-03 07:59:52 -0500

    Fix Windows build script

--- scripts/GenerateTunables_Temp.bat
+++ scripts/GenerateTunables_Temp.bat
@@ -6,6 +6,6 @@ for /f "delims=" %%a in (%filename%) do (
     set "last_line=%%a"
 )
 set "path=!last_line:~52!"
-..\tools\TuningResourceGenerator\TuningResourceGenerator.exe !path!
+..\tools\TuningResourceGenerator\TuningResourceGenerator.exe "!path!"
 del %filename%
 popd
