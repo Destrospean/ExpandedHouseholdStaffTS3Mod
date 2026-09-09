32090ae782a7f9166fb0316feaca09d65fe6ef8b
CreatorAndrew <andrew.noah.mitchell@gmail.com>
2026-09-09 14:27:12 -0500

    Fix bug with female outfits applying to male Sims

--- src/Utils/ServiceUtils.cs
+++ src/Utils/ServiceUtils.cs
@@ -132,9 +132,9 @@ namespace zoeoeAndDestrospean.Utils.ServantRolesMod
 
             List<ulong> mTraits = new List<ulong>();
 
-            uint mValidAges = (uint)(CASAgeGenderFlags.YoungAdult | CASAgeGenderFlags.Adult);
+            uint mValidAges = 0x30u;
 
-            uint mValidGenders = (uint)CASAgeGenderFlags.GenderMask;
+            uint mValidGenders = 0u;
 
             public List<ActiveTopicAction> Actions = new List<ActiveTopicAction>();
 
