372018a84d615ca218f1bef7240a053847f38de1
CreatorAndrew <andrew.noah.mitchell@gmail.com>
2026-09-09 07:42:20 -0500

    Miscellaneous changes

--- src/Main.cs
+++ src/Main.cs
@@ -24,16 +24,20 @@ namespace zoeoeAndDestrospean.ServantRolesMod
     public class Main
     {
         [Tunable]
-        protected static bool kInitTestServices = true;
+        protected static bool kEnableMasterControllerIntegration = true;
 
         [Tunable]
-        protected static bool kInstantiator;
+        protected static bool kInitTestServices = true;
 
         static Main()
         {
             InteractionObjectTypeUtils.InitTypes();
             LoadSaveManager.ObjectGroupsPreLoad += () => Phone.CallForServices.Singleton = CallForServices.Singleton;
             CommonUtils.ReplaceMethod<SocialComponent, Main>("IsInServicePreventingSocialization");
+            if (kEnableMasterControllerIntegration && Array.Exists(AppDomain.CurrentDomain.GetAssemblies(), x => x.GetName().Name == "NRaasMasterController"))
+            {
+                NRaasCompatibility.LoadNRaasMasterControllerModule();
+            }
             if (kInitTestServices)
             {
                 World.sOnWorldLoadFinishedEventHandler += (sender, e) =>
---
+++ src/Utils/NRaasCompatibility.cs
@@ -0,0 +1,43 @@
+﻿using NRaas.CommonSpace.Options;
+using NRaas.MasterControllerSpace.Sims;
+using Sims3.Gameplay;
+using Sims3.Gameplay.Abstracts;
+using Sims3.Gameplay.Actors;
+using Sims3.Gameplay.CAS;
+using Sims3.SimIFace;
+using Sims3.SimIFace.CAS;
+
+namespace zoeoeAndDestrospean.Utils.ServantRolesMod
+{
+    public class NRaasCompatibility
+    {
+        public static void LoadNRaasMasterControllerModule()
+        {
+            OutfitExtensions.EditSpecialOutfit = (sim, specialOutfitKey) =>
+                {
+                    SimDescription simDescription = sim.SimDescription;
+                    if (!simDescription.HasSpecialOutfit(specialOutfitKey))
+                    {
+                        simDescription.AddSpecialOutfit(simDescription.GetOutfit(OutfitCategories.Everyday, 0), specialOutfitKey);
+                    }
+                    OutfitCategories previousOutfitCategory = sim.CurrentOutfitCategory;
+                    int previousOutfitIndex = sim.CurrentOutfitIndex;
+                    simDescription.AddOutfit(simDescription.GetSpecialOutfit(specialOutfitKey), OutfitCategories.Everyday, 0);
+                    simDescription.RemoveSpecialOutfit(specialOutfitKey);
+                    sim.SwitchToOutfitWithoutSpin(OutfitCategories.Everyday, 0);
+                    CASLogic casLogic = CASLogic.GetSingleton();
+                    new Stylist().Perform(new GameHitParameters<GameObject>(sim, sim, GameObjectHit.NoHit));
+                    casLogic.ShowUI += OutfitExtensions.OnShowUI;
+                    while (GameStates.NextInWorldStateId != 0)
+                    {
+                        NRaas.SpeedTrap.Sleep();
+                    }
+                    casLogic.ShowUI -= OutfitExtensions.OnShowUI;
+                    simDescription.AddSpecialOutfit(simDescription.GetOutfit(OutfitCategories.Everyday, 0), specialOutfitKey);
+                    simDescription.RemoveOutfit(OutfitCategories.Everyday, 0, true);
+                    sim.SwitchToOutfitWithoutSpin(previousOutfitCategory, previousOutfitIndex);
+                    return !CASChangeReporter.Instance.CasCancelled;
+                };
+        }
+    }
+}
