74661f4ab6f4f98914d2eeee3a85ef877ed44bb9
CreatorAndrew <andrew.noah.mitchell@gmail.com>
2026-09-15 08:42:40 -0500

    Remove `IsRecurrent` from included services

--- src/Main.cs
+++ src/Main.cs
@@ -86,7 +86,6 @@ namespace Destrospean.ExpandedHouseholdStaff
                                     },
                                 IsLiveInService = true,
                                 IsQuietAroundSleepingSims = true,
-                                IsRecurrent = true,
                                 IsScaredOfBonehilda = true,
                                 Outputs = new List<ServiceUtils.CommodityChange>
                                     {
@@ -128,7 +127,6 @@ namespace Destrospean.ExpandedHouseholdStaff
                                 GetUniformFromName = true,
                                 GetUniformNameCallback = (simDescription) => "career_execchef_" + (simDescription.IsFemale ? "female" : "male") + (simDescription.Elder ? "elder" : ""),
                                 IsLiveInService = true,
-                                IsRecurrent = true,
                                 PotentialTraitCount = 2,
                                 PotentialTraits = new List<TraitNames>
                                     {
